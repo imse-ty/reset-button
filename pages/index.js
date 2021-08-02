0cbd611f2ba954b714737a5f38c11a5f6766ca13
Imsety Taylor <imsety@imsety.com>
2021-08-02 17:39:03 -0400

    Create the big black button

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import { motion } from 'framer-motion';
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-imsetyBlack dark:text-imsetyWhite bg-imsetyWhite dark:bg-imsetyBlack">
       <Head>
         <title>Reset Button</title>
         <meta name="description" content="Always check in with yourself." />
       </Head>
 
-      <h1>Reset Button</h1>
+      <main className="flex  w-full h-full min-w-screen min-h-screen">
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', duration: 0.6, bounce: 0.5 }}
+          className="m-auto"
+        >
+          <motion.div
+            whileTap={{
+              scale: 0.8
+            }}
+            transition={{ type: 'spring', duration: 0.6, bounce: 0.5 }}
+            className="w-60 h-60 md:w-96 md:h-96  bg-imsetyBlack dark:bg-imsetyWhite rounded-full shadow-2xl"
+          />
+        </motion.div>
+      </main>
     </div>
   );
 }
