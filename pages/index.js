84f4dabd5cd237d5eb215e0aa8a27d3a1722cd13
Imsety Taylor <imsety@imsety.com>
2021-08-02 17:23:22 -0400

    Install Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Reset Button</title>
         <meta name="description" content="Always check in with yourself." />
       </Head>
+
+      <h1>Reset Button</h1>
     </div>
   );
 }
